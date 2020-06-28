fa7d09d4ae92bd58f6a6ac0eb686679c2aae9d3e
mironpuzanob <https://github.com/mironpuzanob/>
2020-06-28 19:06:08 +0300

    Updates

---
+++ precache-manifest.6ab005f1852c98f567e91c687eda806d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d680258b5443990b290cf43da35cad6d",
+    "url": "/ambassadorApp/index.html"
+  },
+  {
+    "revision": "7c9303bb11e2881fb9cf",
+    "url": "/ambassadorApp/static/css/2.4911f66b.chunk.css"
+  },
+  {
+    "revision": "7c9303bb11e2881fb9cf",
+    "url": "/ambassadorApp/static/js/2.54050b09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ambassadorApp/static/js/2.54050b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72d9bbeb51741689060",
+    "url": "/ambassadorApp/static/js/main.f89f1d75.chunk.js"
+  },
+  {
+    "revision": "17a43eb23f4255587cc8",
+    "url": "/ambassadorApp/static/js/runtime-main.2f928d8f.js"
+  }
+]);
