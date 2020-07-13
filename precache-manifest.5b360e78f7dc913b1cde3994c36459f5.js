94c3a2b3092d0c1632b847b79fb8cd8208ed26be
mironpuzanob <https://github.com/mironpuzanob/>
2020-07-13 11:42:27 +0300

    Updates

--- precache-manifest.5b360e78f7dc913b1cde3994c36459f5.js
+++ precache-manifest.5b360e78f7dc913b1cde3994c36459f5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c9da49595ee90afd8142048c12e14de",
+    "revision": "40b9e011f7a49a2579ce3c2f1bff5403",
     "url": "/ambassadorApp/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ambassadorApp/static/js/2.b7ec3284.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "97c8348dff367d0b5f3e",
-    "url": "/ambassadorApp/static/js/main.80d08d85.chunk.js"
+    "revision": "767c5bee035296516562",
+    "url": "/ambassadorApp/static/js/main.ffa5ed2a.chunk.js"
   },
   {
     "revision": "17a43eb23f4255587cc8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ambassadorApp/precache-manifest.2d62d658f88dbb62a83fcf90f92553ea.js"
+  "/ambassadorApp/precache-manifest.5b360e78f7dc913b1cde3994c36459f5.js"
 );
 
 self.addEventListener('message', (event) => {
