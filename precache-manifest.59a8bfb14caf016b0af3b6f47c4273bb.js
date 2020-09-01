8fb1f7ce9c0df0bfbb908677dd3a4a84771b8d8f
Dimovy <55894047+Dimovy@users.noreply.github.com>
2020-09-01 13:46:26 +0300

    Updates

---
+++ precache-manifest.59a8bfb14caf016b0af3b6f47c4273bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4218d77f2a14bc1dc77d2e451b7cc0b4",
+    "url": "/ambassadorApp/index.html"
+  },
+  {
+    "revision": "107738fd095cd8522f0c",
+    "url": "/ambassadorApp/static/css/2.4911f66b.chunk.css"
+  },
+  {
+    "revision": "6b5eb08a0bca6470fa3e",
+    "url": "/ambassadorApp/static/css/main.c75ad9db.chunk.css"
+  },
+  {
+    "revision": "107738fd095cd8522f0c",
+    "url": "/ambassadorApp/static/js/2.953ea1f7.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/ambassadorApp/static/js/2.953ea1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5eb08a0bca6470fa3e",
+    "url": "/ambassadorApp/static/js/main.d603bfa1.chunk.js"
+  },
+  {
+    "revision": "17a43eb23f4255587cc8",
+    "url": "/ambassadorApp/static/js/runtime-main.2f928d8f.js"
+  }
+]);
